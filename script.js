3b646e8a93bcd903047d33a2d3a96512a34d9256
Sumon Ahasan <sumon.ahasan@stonybrook.edu>
2024-07-26 17:36:09 -0400

    first push

---
+++ script.js
@@ -0,0 +1,13 @@
+function togglesbuPop() {
+    document.getElementById('sbuPop').classList.toggle("active");
+}
+function togglelatinPop() {
+    document.getElementById('latinPop').classList.toggle("active");
+}
+function openMenu() {
+    document.getElementById('sidemenu').style.right="0";
+}
+function closeMenu() {
+    document.getElementById('sidemenu').style.right="-200px";
+}
+
